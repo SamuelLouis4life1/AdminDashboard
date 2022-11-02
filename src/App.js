66a942644f44da351d722cbebe600c27f30bb2b4
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2022-11-02 10:44:55 -0700

    Pages files

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ import './App.css';
 // import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
+    const { activeMenu } = true;
 //   const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+
 
 //   useEffect(() => {
 //     const currentThemeColor = localStorage.getItem('colorMode');
@@ -43,6 +45,7 @@ const App = () => {
                 type="button"
                 // onClick={() => setThemeSettings(true)}
                 // style={{ background: currentColor, borderRadius: '50%' }}
+                style={{ background: 'blue', borderRadius: '50%' }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
@@ -51,22 +54,24 @@ const App = () => {
             </TooltipComponent>
           </div>
 
-          {/* {activeMenu ? (
+          {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-              <Sidebar />
+                Sidebar
+              {/* <Sidebar /> */}
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
-              <Sidebar />
+                Sidebar
+              {/* <Sidebar /> */}
             </div>
-          )} */}
+          )}
           
           <div
-            // className={
-            //   activeMenu
-            //     ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-            //     : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-            // }
+            className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2'
+            }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               {/* <Navbar /> */}
